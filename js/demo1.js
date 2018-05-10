b38b7e895f6d8d0facde0a7e0af4b92997e12119
iMassim0 <i.max@live.fr>
2018-05-10 19:07:04 +0200

    modif supp css et final code

--- js/demo1.js
+++ js/demo1.js
@@ -9,6 +9,8 @@
  * http://www.codrops.com
  */
 
+
+
 {
     const chars = ['$','%','#','@','&','(',')','=','*','/'];
     const charsTotal = chars.length;
@@ -83,3 +85,9 @@
         }
     });
 }
+
+var f = document.getElementById('foo');
+document.addEventListener('click', function(ev){
+    f.style.transform = 'translateY('+(ev.clientY-25)+'px)';
+    f.style.transform += 'translateX('+(ev.clientX-25)+'px)';
+},false);
